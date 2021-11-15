6c2fa5f5073af6b28d0d18f083c94b3ffe811d3a
Oliver Oliverio <oliveroliverio@gmail.com>
2021-11-15 14:18:27 -0800

    20211115: part01 created functions to send JSON action/listen messages to alpaca.  but doesn't work...

--- Part01/stream.py
+++ Part01/stream.py
@@ -7,16 +7,12 @@ def on_open(ws):
         "action": "authenticate",
         "data": {"key_id": config.API_KEY, "secret_key": config.SECRET_KEY}
     }
+    # send auth data to alpaca
     ws.send(json.dumps(auth_data))
-
-#     channel_data = {
-#         "action": "subscribe",
-#         "params": TICKERS
-#     }
-
-#     ws.send(json.dumps(channel_data))
-
-
+    # create listen_message object that youll send to alpaca
+    listen_message = {"action": "listen", "data": {"streams": ["T.SPY"]}}
+    # send listen_message to alpaca, which will send back stream data
+    ws.send(json.dumps(listen_message))
 def on_message(ws, message):
     print("received a message")
     print(message)
@@ -26,5 +22,5 @@ def on_message(ws, message):
 
 socket = "wss://data.alpaca.markets/stream"
 
-ws = websocket.WebSocketApp(socket, on_open=on_open)
+ws = websocket.WebSocketApp(socket, on_open=on_open, on_message=on_message)
 ws.run_forever()
